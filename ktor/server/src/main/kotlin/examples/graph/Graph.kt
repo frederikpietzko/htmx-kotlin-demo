c7bae9935b5fc8bc3db4740a129e5c32daba8df1
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-11-23 15:28:56 +0100

    datatable in graphs

--- ktor/server/src/main/kotlin/examples/graph/Graph.kt
+++ ktor/server/src/main/kotlin/examples/graph/Graph.kt
@@ -10,6 +10,7 @@ import kotlinx.html.div
 import kotlinx.html.id
 import kotlinx.html.unsafe
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.io.toHTML
 import org.jetbrains.kotlinx.kandy.dsl.plot
 import org.jetbrains.kotlinx.kandy.letsplot.export.toHTML
 import org.jetbrains.kotlinx.kandy.letsplot.feature.layout
@@ -22,13 +23,14 @@ fun Application.graph() {
   routing {
     get("/graph") {
       val visitors = VisitorManagement.allVisitors()
-      val (bars, pie) = graphs(visitors)
+      val (bars, pie, data) = graphs(visitors)
       call.respondBaseTemplate(CommonPage("Graph Showcase")) {
         children {
           div("flex flex-col p-4 gap-3") {
             id = "graph"
             unsafe { +bars }
             unsafe { +pie }
+            unsafe { +data }
           }
         }
       }
@@ -36,7 +38,7 @@ fun Application.graph() {
   }
 }
 
-private fun graphs(visitors: List<Visitor>): Pair<String, String> {
+private fun graphs(visitors: List<Visitor>): Triple<String, String, String> {
   val categoies = listOf(
     "like Javascipt and are wrong",
     "knows Kotlin",
@@ -58,10 +60,11 @@ private fun graphs(visitors: List<Visitor>): Pair<String, String> {
       y(counts, "count")
     }
   }.toHTML()
-  val pie = dataFrameOf(
+  val data = dataFrameOf(
     "category" to categoies,
     "counts" to counts,
-  ).plot {
+  )
+  val pie = data.plot {
     pie {
       slice("counts")
       fillColor("category")
@@ -75,5 +78,7 @@ private fun graphs(visitors: List<Visitor>): Pair<String, String> {
       style(Style.Void)
     }
   }.toHTML()
-  return Pair(bars, pie)
+  return Triple(
+    bars, pie, data.toHTML().toString()
+  )
 }
